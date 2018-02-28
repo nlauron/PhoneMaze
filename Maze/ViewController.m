e10661f44d2a8dd29d3478895e136cfb2a27b4af
Matthew Taylor <mtaylor8878@gmail.com>
2018-02-28 14:04:04 -0800

    Initial setup

---
+++ Maze/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Maze
+//
+//  Created by Matthew Taylor on 2018-02-28.
+//  Copyright © 2018 Matthew Taylor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () {
+    Renderer *glesRenderer;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Maze/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Maze
+//
+//  Created by Matthew Taylor on 2018-02-28.
+//  Copyright © 2018 Matthew Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
